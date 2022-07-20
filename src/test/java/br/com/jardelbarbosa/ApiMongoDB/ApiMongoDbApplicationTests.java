018a37607b6b5d6d5a36c19cf80021b4458c6604
Jardel894 <jardelsantos894@gmail.com>
2022-07-20 10:18:30 -0300

    Projeto Criado

---
+++ src/test/java/br/com/jardelbarbosa/ApiMongoDB/ApiMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jardelbarbosa.ApiMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
